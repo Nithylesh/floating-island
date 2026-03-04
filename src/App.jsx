5b6aa2610cf785fb6f599a2175fd02f76d8128ba
Nithylesh <nithylesh683@gmail.com>
2026-03-04 23:31:11 +0530

    Done with the code but it looks so uglyyyyyyyyyyyy

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,10 @@
 import { Canvas } from '@react-three/fiber';
 import {SceneContainer} from './SceneContainer';
-import FloatingIsland from './FloatingIsland';
-import Rocks from './Rocks';
-import Portal from './Portal';
-import FloatingRocks from './FloatingRocks';
-import Trees from './Trees';
-import Words from './Words';
-import Grass from './Grass';
-import { Float } from '@react-three/drei';
+
 function App() {
-    return <Canvas
-        gl={{
-            stencil: true
-        }}>
+    return <Canvas>
         // double check if this should be inside 
         <SceneContainer/>
-        <Float 
-        speed={0.5} 
-        rotationIntensity={0.6} 
-        floatIntensity={0.6}>
-
-        <SceneContainer/>
-        <FloatingIsland/>
-        <Rocks/>
-        <Portal/>
-        <FloatingRocks/>
-        <Trees/>
-        <Words/>
-        <Grass/>
-        </Float>
     </Canvas>;
 }
 
